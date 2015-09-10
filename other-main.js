3004ba7214e532abcd3ea04324f232ee8bbbafd5
Student <student@RU-019.local>
2015-09-10 16:53:00 -0600

    first commit

---
+++ other-main.js
@@ -0,0 +1,20 @@
+angular.module('myApp', [])
+angular.module('myApp')
+
+var mainController = function($scope){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+angular.module('myApp').controller('mainController', ['$scope', mainController])
